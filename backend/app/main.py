487481fce34e11c07c0d5fdce114e3d923ad8fd0
shurxet <s.shurupov.88@mail.ru>
2024-11-22 11:58:34 +0300

    update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,18 +3,15 @@ from fastapi import FastAPI
 from .api import api_router
 from .core.database import init_db
 
-
 app = FastAPI(
     title="Insurance Service",
-    description="API для расчёта стоимости страхования и управления тарифами",
+    description="REST API для расчёта стоимости страхования и управления тарифами",
     version="1.0.0",
 )
 
 app.include_router(api_router)
 
 
-init_db()
-
-# @app.on_event("startup")
-# def on_startup():
-#     init_db()
+@app.on_event("startup")
+def on_startup():
+    init_db()
